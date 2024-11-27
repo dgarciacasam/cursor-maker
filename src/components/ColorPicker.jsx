fa9d24cc981145aa27ce435da30bfbd8937c0b2e
dgarciacasam <dgarciacasam@gmail.com>
2024-11-27 10:07:18 +0100

    feat: Ahora los campos input de color solo aceptarán colores hexadecimales válidos

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,6 +1,15 @@
 import { HexColorPicker } from 'react-colorful'
 
 export const ColorPicker = ({ label, color, setColor }) => {
+  const validateHexColor = (e) => {
+    const value = e.target.value
+    const hexPattern = /^#([A-Fa-f0-9]{0,6}|[A-Fa-f0-9]{0,3})$/
+
+    if (value === '' || hexPattern.test(value)) {
+      setColor(value)
+    }
+  }
+
   return (
     <div className='w-72 h-72'>
       <span className='text-2xl my-3'>{label}</span>
@@ -9,7 +18,7 @@ export const ColorPicker = ({ label, color, setColor }) => {
         className='mt-2 p-2 rounded-lg w-full text-xl tracking-wider bg-[#121212] text-white'
         type='text'
         value={color}
-        onChange={(e) => setColor(e.target.value)}
+        onChange={validateHexColor}
       />
     </div>
   )
